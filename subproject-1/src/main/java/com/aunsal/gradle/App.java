272961da1904284bce5248e6e1e7517f4eab32cb
Aziz Unsal <azizunsal@Azizs-MacBook-Pro.local>
2022-11-30 17:23:27 +0300

    Improved the project structure by making some folders visible.

--- subproject-1/src/main/java/com/aunsal/gradle/App.java
+++ subproject-1/src/main/java/com/aunsal/gradle/App.java
@@ -1,7 +1,11 @@
 package com.aunsal.gradle;
 
 public class App {
+    public static String sayHi() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(App.sayHi());
     }
 }
---
+++ subproject-1/src/test/java/com/aunsal/gradle/AppTest.java
@@ -0,0 +1,21 @@
+package com.aunsal.gradle;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class AppTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void sayHi() {
+    }
+}
