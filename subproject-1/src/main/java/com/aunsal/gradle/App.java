ca3601f3d30920f270818720566633e9dc214758
Aziz Unsal <azizunsal@Azizs-MacBook-Pro.local>
2022-11-30 17:10:15 +0300

    Modified a IDE related file.

--- subproject-1/src/main/java/com/aunsal/gradle/App.java
+++ subproject-1/src/main/java/com/aunsal/gradle/App.java
@@ -2,6 +2,6 @@ package com.aunsal.gradle;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World!");
     }
 }
